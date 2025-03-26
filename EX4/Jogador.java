15d65e38b053005309bba673b37911a802d8e166
André Caires <134800035+acairesm@users.noreply.github.com>
2025-03-26 15:39:49 -0300

    Update Jogador.java

--- EX4/Jogador.java
+++ EX4/Jogador.java
@@ -2,24 +2,45 @@ import java.util.Scanner;
 
 public class Jogador {
 
-
     String nome;
     int pontuacao;
     int nivel;
 
     public Jogador(String nome, int pontuacao, int nivel) {
-
         this.nome = nome;
         this.pontuacao = pontuacao;
         this.nivel = nivel;
     }
 
+    public static Jogador addinfo(Scanner leitor) {
+        System.out.println("Qual o nome do jogador?");
+        String nome = leitor.nextLine();
 
+        System.out.println("Qual a pontuação inicial?");
+        int pontuacao = leitor.nextInt();
 
+        System.out.println("Qual o nível inicial?");
+        int nivel = leitor.nextInt();
+        leitor.nextLine();
 
+        return new Jogador(nome, pontuacao, nivel);
+    }
 
+    public void exibirInfo() {
+        System.out.println("Nome: " + nome + " | Pontuação: " + pontuacao + " | Nível: " + nivel);
+    }
 
+    public void atualizarPontuacao(Scanner leitor) {
+        System.out.println("Digite a nova pontuação:");
+        this.pontuacao = leitor.nextInt();
+        leitor.nextLine();
+        System.out.println("Pontuação alterada!");
+    }
 
-
-
+    public void atualizarNivel(Scanner leitor) {
+        System.out.println("Digite o novo nível:");
+        this.nivel = leitor.nextInt();
+        leitor.nextLine();
+        System.out.println("Nível alterado!");
+    }
 }
