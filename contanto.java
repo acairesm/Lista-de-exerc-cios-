efc3c8c086be2f2eefb0cc8e3ec1feb5030ff97b
IsaacLima10 <pedroisaac.gomes@icloud.com>
2025-03-31 18:51:18 -0300

    Add files via upload

---
+++ contanto.java
@@ -0,0 +1,22 @@
+    class contato {
+    private String nome;
+    private String telefone;
+
+    public contato(String nome, String telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+
+    public String toString() {
+        return "Nome: " + nome + ", Telefone: " + telefone;
+    }
+}
